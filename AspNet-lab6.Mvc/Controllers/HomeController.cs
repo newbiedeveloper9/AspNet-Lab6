e87e3123189c5a59dfe3262f6139f619b4fa590e
Michał Wojciechowski <kontogry96@gmail.com>
2022-01-16 00:37:32 +0100

    Add project files.

---
+++ AspNet-lab6.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using AspNet_lab6.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNet_lab6.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult WeatherForecastListView()
+        {
+            var client = new HttpClient();
+            var response = client.GetFromJsonAsync<IEnumerable<WeatherForecastViewModel>>(
+                "https://localhost:44330/api/WeatherForecast")
+                      .GetAwaiter()
+                      .GetResult();
+
+            return View(response);
+        }
+    }
+}
---
+++ AspNet-lab6.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNet_lab6.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
